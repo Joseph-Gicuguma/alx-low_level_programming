3d265c12f0edbe5a98f45ac134a7f842002e487e
Joe <gicugumajoseph@gmail.com>
2022-07-01 10:39:40 -0700

    alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,6 +9,13 @@ int main(void)
 char lower_case;
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 putchar(lower_case);
+{
+if (alphabets == 'q')
+continue;
+else if (alphabets == 'e')
+continue;
+putchar(alphabets);
+}
 putchar('\n');
 return (0);
 }
