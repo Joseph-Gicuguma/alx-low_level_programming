7a0d335e6cb427562a7cbfb138ddc529d5c3e39f
Joe <gicugumajoseph@gmail.com>
2022-07-01 11:10:54 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+s (16 sloc)  178 Bytes
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
